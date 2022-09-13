0716cc854cc80c6efd0158a700e07848fecaedde
vjeanty02 <vjeanty02@gmail.com>
2022-09-12 21:43:05 -0500

    File 4-101

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the code
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always return 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
